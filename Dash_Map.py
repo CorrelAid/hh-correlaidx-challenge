af6be695542688ccee21aac3373b62275587c844
EvaJau <evaj@posteo.de>
2020-09-09 21:55:23 +0200

    update readme and descripitions

--- Dash_Map.py
+++ Dash_Map.py
@@ -21,7 +21,7 @@ df3.replace(0, np.nan, inplace=True)
 
 # Add columns for id_nuts1 and name_nuts1
 regions = get_regions()
-regions_nuts1 = regions[regions.level=="nuts1"]["name"]   # Get id and names of regions on nuts1 level
+regions_nuts1 = regions[regions.level == "nuts1"]["name"]   # Get id and names of regions on nuts1 level
 df3["id_nuts1"] = [str(x)[:2] for x in df3.id_nuts3]   # Add column with id_nuts1
 df3 = pd.merge(df3, regions_nuts1, how='left', left_on="id_nuts1", right_index=True)  # Add column with nuts1 name
 df3.rename(columns=lambda x: x+"_nuts1" if x in ["id", "name"] else x, inplace=True)   # Rename column to name_nuts1
@@ -47,8 +47,8 @@ scaled_df1=pd.concat([scaled_df1,scaled_cols], axis=1)
 
 
 #importing json containing Landkreis borders
-landkreise = gpd.read_file(f"landkreise_simplify200.geojson")
-bundeslaender = gpd.read_file(f"bundeslaender_simplify200.geojson")
+landkreise = gpd.read_file("landkreise_simplify200.geojson")
+bundeslaender = gpd.read_file("bundeslaender_simplify200.geojson")
 
 # defining features for input
 features = sorted(list(df3.drop(['id_nuts1','name_nuts1','id_nuts3','name_nuts3','year'],axis=1).columns))
@@ -58,12 +58,44 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
+
+    html.Div([
+        dcc.Markdown("### Child Well-Being in Germany"),
+        dcc.Markdown("This project was conducted in the context of the [CorrelAid](https://correlaid.org/) local chapter challenge"
+                     " July - September 2020 by the [local chapter of Hamburg](https://correlaid.org/correlaid-x/hamburg/)."
+                     " Contributors: Martin Wong, Vivika Wilde, Sarah Wenzel, Drenizë Rama, Long Nguyen, Trisha Nath, Christine Martens, "
+                     "Mauricio Malzer, Andre Kochanke, Eva Jaumann"),
+        dcc.Markdown("Germany belongs to the rich countries, nevertheless this is not a guarantee that all children and adolescents grow up in happiness."),
+        dcc.Markdown("There are many different factors which contribute to the well-being of a child, main categories according to [UNICEF](https://www.unicef.org/media/files/ChildPovertyReport.pdf) are:"
+                     " Material well-being"
+                     ", Health and safety"
+                     ", Educational well-being"
+                     ", Family and peer relationships "
+                     ", Behaviours and risks"
+                     ", Subjective well-being"),
+        dcc.Markdown("**Our goal of this project was not to calculate one single metric to evaluate happiness, but to raise awareness to"
+                     " the topic and how many different factors influence children.**"),
+        dcc.Markdown("**For more information please also check the [README](https://github.com/CorrelAid/hh-correlaidx-challenge/blob/master/README.md).**"),
+        dcc.Markdown("#### Disclaimer"),
+        dcc.Markdown("Be careful when interpreting the averages of multiple factors"),
+        dcc.Markdown("The interactive map displays a standardized and normalized average across all features selected via the checkboxes."
+                     " Features include information with some relation to child well-being and are intended to be selected according to the user's specific information interest"
+                     " The combination of features is up to the user and the interpretation needs to be made with caution. The data has not been weighted and some factors may be over-, under- or misrepresented"
+                     " Features like *population* are included for illustration purposes, even though they are not directly linked to child well-being"),
+        dcc.Markdown("Also be aware that there might be data gaps - not for every year every data is available.")
+    ],
+        style={'width': '100%',
+               'display': 'block',
+               'padding': 30,
+               'font-size': 12},
+    ),
+
     html.Div([
         html.Div([
             dcc.Markdown('###### Level:'),
             dcc.RadioItems(
                 id='level_selection',
-                options=[{'label': 'Bundesländer', 'value':'nuts1'},
+                options=[{'label': 'Bundeslaender', 'value':'nuts1'},
                          {'label': 'Landkreise', 'value':'nuts3'}],
                 value = 'nuts3',
             ),
@@ -99,27 +131,65 @@ app.layout = html.Div([
     ]),
     html.Div([
         dcc.Markdown("### Variable Descriptions"),
-        dcc.Markdown("###### Crime suspects <14 per 1000"),
-        dcc.Markdown("Number of crime suspects from 0-13 (incl.) years in given NUTS-3 region. Includes all possible "
-                     "types of crimes (please refer to this list for a full explanation of included crimes). The value "
-                     "shows the negative of crimes per 1000 population (e.g. -1.4 indicates 1.4 suspects per 1000 "
-                     "population in that NUTS-3 region in given year). Data is taken from BKA."),
-        dcc.Markdown("###### Crime suspects 14-<18 per 1000"),
-        dcc.Markdown("Number of crime suspects from 14-17 (incl.) years in given NUTS-3 region. Includes all possible "
-                     "types of crimes (please refer to this list for a full explanation of included crimes). The value "
-                     "shows the negative of crimes per 1000 population (e.g. -1.4 indicates 1.4 suspects per 1000 "
-                     "population in that NUTS-3 region in given year). Data is taken from BKA."),
-        dcc.Markdown("###### Crime victims <14 per 1000 "),
-        dcc.Markdown("Number of crime victims from 0-13 (incl.) years in given NUTS-3 region. "
-                     "Includes all possible types of crimes (please refer to this list for a full explanation of "
-                     "included crimes). The value shows the negative of victims per 1000 population (e.g. -1.74 "
-                     "indicates 1.74 victims per 1000 population in that NUTS-3 region in given year). Data is taken "
-                     "from BKA."),
+        dcc.Markdown("**Some of the factors cover multiple categories, for example 'Children affected by divorce' "
+                     "will not only affect the subjective, but also the material well-being.** Therefore, "
+                     "consider the categorization only as a guide."),
+        dcc.Markdown("#### Material well-being"),
+        dcc.Markdown("###### Disposable income per inhabitant"),
+        dcc.Markdown(" Average amount of money available to private households for consumption or savings per inhabitant. "
+                     "Data are queried using datenguidepy."),
+        dcc.Markdown("###### Recipients of minimum social security benefits"),
+        dcc.Markdown("Recipients of minimum social security benefits (includes basic security in old age and gainful "
+                     "employment benefit). "
+                     "Value is inverted. Data is taken from datenguidepy."),
+        dcc.Markdown("###### Unemployment rate"),
+        dcc.Markdown("Unemployed persons as a share of the civilian labor force averaged over the year. Value is "
+                     "inverted. Data is taken from datenguidepy."),
+
+
+
+        dcc.Markdown("#### Health and safety"),
+        dcc.Markdown("###### Childcare facilities"),
+        dcc.Markdown("The indicator shows number of childcare facilities for 0-14 years "
+                     "children in given NUTS-3 region. Data is taken from datenguidepy."),
+        dcc.Markdown("###### Childcare workers per inhabitant"),
+        dcc.Markdown("Number of persons working in publicly funded day care facilities for children divided by "
+                     "population size. Data are queried using datenguidepy."),
         dcc.Markdown("###### Crime victims 14-<18 per 1000"),
         dcc.Markdown("Number of crime victims from 14-17 (incl.) years in given NUTS-3 region. Includes all possible "
                      "types of crimes (please refer to this list for a full explanation of included crimes). The value "
                      "shows the negative of victims per 1000 population (e.g. -1.74 indicates 1.74 victims per 1000 "
                      "population in that NUTS-3 region in given year). Data is taken from BKA."),
+        dcc.Markdown("###### Crime victims <14 per 1000"),
+        dcc.Markdown("Number of crime victims from 0-13 (incl.) years in given NUTS-3 region. "
+                     "Includes all possible types of crimes (please refer to this list for a full explanation of "
+                     "included crimes). The value shows the negative of victims per 1000 population (e.g. -1.74 "
+                     "indicates 1.74 victims per 1000 population in that NUTS-3 region in given year). Data is taken "
+                     "from BKA."),
+        dcc.Markdown("###### Facilities for youth welfare services"),
+        dcc.Markdown("The number of youth welfare facilities include facilities from "
+                     "both public and private sponsors. Both facilities (excluding day care facilities for children) "
+                     "include: youth work institutions, institutions of youth social work, family support institutions, common "
+                     "forms of housing for mothers / fathers and children, education, youth and family advice centers, "
+                     "institutions for help with education and help for young people, facilities for staff training, "
+                     "facilities for young people with disabilities.  (source: datenguide)"),
+        dcc.Markdown("###### Facilities with integrated childcare"),
+        dcc.Markdown("The indicator shows the number of facilities with an integrated childcare in "
+                     "given NUTS-3 region. Data is taken from datenguidepy."),
+        dcc.Markdown("###### Hospital beds per 1000 inhabitants"),
+        dcc.Markdown("How many hospital beds were set up per 1000 inhabitants on annual average. Note: a high bed density "
+                     "can also result from supraregionally oriented centers (e.g. university hospitals). "
+                     "Data are queried using datenguidepy."),
+
+
+
+        dcc.Markdown("#### Educational well-being"),
+        dcc.Markdown("###### Children 0-2 year in daycare(%)"),
+        dcc.Markdown("The indicator shows percentage of children (0-2 years) having daycare facilities "
+                     "of total children of same age group in given NUTS-3 region. Data is taken from datenguidepy."),
+        dcc.Markdown("###### Children 3-5 year in care(%)"),
+        dcc.Markdown("The indicator shows percentage of children (3-5 years) having daycare facilities "
+                     "of total children of same age group in given NUTS-3 region. Data is taken from datenguidepy."),
         dcc.Markdown("###### School leavers with higher education qualification"),
         dcc.Markdown("The indicator shows what percentage of all school leavers finish school with a qualification to "
                      "continue in higher education. Data is taken from datenguidepy."),
@@ -127,7 +197,48 @@ app.layout = html.Div([
         dcc.Markdown("The indicator shows what percentage of all school leavers finish school prematurely or without a "
                      "qualification and thus have the most unfavourable prerequisites for entering job formation. The "
                      "value shows the negative (e.g. -8.8 indicates 8.8% of school leavers left without high school "
-                     "certificate). Data is taken from datenguidepy.")
+                     "certificate). Data is taken from datenguidepy."),
+
+
+        dcc.Markdown("#### Family and peer relationships"),
+        dcc.Markdown("###### Average age of local population"),
+        dcc.Markdown("The indicator shows average age of local population in given NUTS-3 region. "
+                     "Data is taken from datenguidepy."),
+        dcc.Markdown("###### Average age of mother 1st birth"),
+        dcc.Markdown("The indicator shows average age of mother at giving birth of "
+                     "first child in given NUTS-3 region. Data is taken from datenguidepy."),
+        dcc.Markdown("###### Child population(%)"),
+        dcc.Markdown("The indicator shows percentage of children (0-17 years) of "
+                     "total population in given NUTS-3 region. Data is taken from datenguidepy."),
+
+
+
+        dcc.Markdown("#### Behaviours and risks"),
+        dcc.Markdown("###### Crime suspects 14-<18 per 1000"),
+        dcc.Markdown("Number of crime suspects from 14-17 (incl.) years in given NUTS-3 region. Includes all possible "
+                     "types of crimes (please refer to this list for a full explanation of included crimes). The value "
+                     "shows the negative of crimes per 1000 population (e.g. -1.4 indicates 1.4 suspects per 1000 "
+                     "population in that NUTS-3 region in given year). Data is taken from BKA."),
+        dcc.Markdown("###### Crime suspects <14 per 1000"),
+        dcc.Markdown("Number of crime suspects from 0-13 (incl.) years in given NUTS-3 region. Includes all possible "
+                     "types of crimes (please refer to this list for a full explanation of included crimes). The value "
+                     "shows the negative of crimes per 1000 population (e.g. -1.4 indicates 1.4 suspects per 1000 "
+                     "population in that NUTS-3 region in given year). Data is taken from BKA."),
+
+
+
+        dcc.Markdown("#### Subjective well-being"),
+        dcc.Markdown("###### Children affected by divorce per inhabitant"),
+        dcc.Markdown(" Number of children whose parents divorced divided by population size. "
+                     "Data are queried using datenguidepy. Values are inverted."),
+        dcc.Markdown("###### Father in parental benefit(%)"),
+        dcc.Markdown("The indicator shows information on fathers' participation in parental "
+                     "benefits in given NUTS-3 region. Data is taken from datenguidepy."),
+        dcc.Markdown("###### Share of sports, leisure and recreation areas in total area (%)"),
+        dcc.Markdown("Share of sports, leisure and recreation areas in total area (%): “Proportion of sports, leisure and recreation areas in relation to the "
+                     "total area on December 31st. The sports, leisure and recreation area is "
+                     "divided by the total area and multiplied by 100.” (source: datenguide)"),
+
     ],
         style={'width': '100%',
                'display': 'block',
@@ -135,6 +246,7 @@ app.layout = html.Div([
                'font-size': 12},
     ),
 ])
+
 
 @app.callback(
     Output('graph-with-slider', 'figure'),
@@ -170,7 +282,7 @@ def update_figure(selected_level, selected_features, selected_year):
                                center={"lat": 51.1633, "lon": 10.4477},
                                animation_frame="year",
                                hover_name=str("name_"+selected_level),
-                               #hover_data={feature: True for feature in selected_features},
+                               # hover_data={feature: True for feature in selected_features},
                                mapbox_style="carto-positron",
                                zoom=5,
                                opacity=1)
--- README.md
+++ README.md
@@ -1,19 +1,124 @@
-# Background to correlaidx-challenge-hh
+# Child Well-Being in Germany 
 
-- ...
+---
+This project was conducted in the context of the [CorrelAid](https://correlaid.org/) local chapter challenge 
+July - September 2020 by the [local chapter of Hamburg](https://correlaid.org/correlaid-x/hamburg/).
+
+Contributors: 
+Martin Wong, Vivika Wilde, Sarah Wenzel, Drenizë Rama, Long Nguyen, Trisha Nath, Christine Martens, 
+Mauricio Malzer, Andre Kochanke, Eva Jaumann
+---
+
+Germany belongs to the rich countries, nevertheless this is not a guarantee that all children and adolescents
+grow up in happiness. 
+
+UNICEF studies [1,2] showed that even wealthy countries have room to improvement to ensure well-being for their youngest
+citizens. There are many different factors which contribute to the well-being of a child, main categories according to UNICEF[2] are
+
+* Material well-being
+* Health and safety
+* Educational well-being
+* Family and peer relationships 
+* Behaviours and risks
+* Subjective well-being
+
+To each category different metrics are used, so that it is challenging to calculate one single value which reports
+the well-being of a country.
+---
+**NOTE**
+
+Our goal of this project was not to calculate one single metric to evaluate happiness, but to raise awareness to
+the topic and how many different factors influence children. Please check also our [notes](#notes-on-the-interactive-map).
+
+---
+
+With our visualisation we want to be able to show the difference in german regions on different factors which directly
+or indirectly touch the life of young people. 
+
+It is not only possible to look at regional differences, but also how the metrics evolved in the last years. You can discover
+the factors you are mostly interested.
+
+## Data
+
+We used data from the [datenguide](https://github.com/CorrelAid/datenguide-python), which is a package to easily
+access German publicly available [regional statistics](https://www.regionalstatistik.de/genesis/online/logon). 
+In addition, [German police crime statistics](https://www.bka.de/DE/AktuelleInformationen/StatistikenLagebilder/PolizeilicheKriminalstatistik/pks_node.html)
+were used.
+
+Data are on NUTS-3 level. "Germany's NUTS-3 regions are generally districts known as Kreise or as kreisfreie Städte." [3]
+
+
+## Visualisation
+
+The map shows different, selectable factors on either state or nuts-3 level. The interactive map was build as a dash application.
+If you don't see a result, data are missing for this factor and this year.
+
+Please have a look at the [video](https://www.awesomescreenshot.com/video/910729?key=ec527c8315a26c4d3ce8a8a85a35de47) to
+see how the visualisation looks like.
+
+Following screenshots give a first impression:
+
+### Daycare 0-2 years (2019)
+The indicator shows percentage of children (0-2 years) having daycare facilities of total children of same age group.
+
+![daycare 2019](screenshots/daycare2019.png)
+
+It is interesting to see the differences between west and east Germany even 30 years after the fall
+of the Berlin wall. These are still caused by a different development of child care in the different
+political systems.[4]
+
+### Age of mother 1st child 2018
+The indicator shows average age of mother at giving birth of the first child.
+
+![mother](screenshots/motherage2018.png)
+
+This figure indicates that mothers in cities as Munich or Frankfurt are older than mothers in rural
+areas when giving birth to their first child. A study[5] came to the conclusion, that mothers who are younger
+than 25 have a lower lifetime income of more than two years of annual labor income. Therefore, it is
+also a financial aspect for the child's well-being indirectly.
+
+### Unemployment 2019
+
+Unemployed persons as a share of the civilian labor force averaged over the year. 
+
+![mother](screenshots/unemployment2019.png)
+
+Another material aspect shows the unemployment rate, as money is a key to access to education, 
+culture and more. 
+
+These were some examples to illustrate the map, there are more insights hidden!
+
+## References
+
+[1] https://www.unicef-irc.org/publications/pdf/Report-Card-16-Worlds-of-Influence-child-wellbeing.pdf (last accessed 2020-09-09)
+
+[2] https://www.unicef.org/media/files/ChildPovertyReport.pdf (last accessed 2020-09-09)
+
+[3] https://www.destatis.de/Europa/EN/Methods/Classifications/OverviewClassification_NUTS.html (last accessed 2020-09-09)
+
+[4] https://www.dji.de/fileadmin/user_upload/bibs2018/WEB_DJI_ExpertiseDeutschland.pdf (last accessed 2020-09-09)
+
+[5] https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4723246/ (last accessed 2020-09-09)
+
+COVID-19 and children mental health: https://www.weforum.org/agenda/2020/05/covid-19-is-hurting-childrens-mental-health (last accessed 2020-09-09)
+
 
 # Notes on the interactive map
 
 - The interactive map displays a standardized and normalized average across all features selected via the checkboxes
-- Features include information with some relation to child wellbeing and are intended to be selected according to the user's specific information interest
+- Features include information with some relation to child well-being and are intended to be selected according to the user's specific information interest
 - The combination of features is up to the user and the interpretation needs to be made with caution. The data has not been weighted and some factors may be over-, under- or misrepresented
-- Features like "population" are included for illustration purposes, even though they are not directly linked to child wellbeing
+- Features like "population" are included for illustration purposes, even though they are not directly linked to child well-being
 
-# Instructions for running the interactive map
+# Instructions for running the interactive map (dash app)
+
+- Requirements: python 3.8 and [datenguide python package](https://github.com/CorrelAid/datenguide-python) have to 
+be installed
 
 - Clone the repository to your local file system
 - Set up an environment based on the included .yml file (e.g. by importing it to Anaconda or manually installing the libraries listed in the requirements file)
-- Run the Dash_Map.py file in the terminal or via any IDE (e.g PyCharm)
+- Run the Dash_Map.py file in the terminal or via any IDE (e.g PyCharm) 
+In case of errors please check if python 3.8 is used, as it is not working with versions below
 - Open the web application via the provided localhost URL (e.g. http://127.0.0.1:8050/)
 
 To add new datasets to the map: Place the file in pickle format in the folder "data_pickles" and the app will include it in the next run
